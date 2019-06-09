8c8dc172d195392ee9ee9ac0d996ad7b90bcf530
Heather Peri <heatherhperi@gmail.com>
2019-06-09 11:20:26 -0400

    updated server.js and added the express package

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+
+var db = require("./models");
+
+var app = express();
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+var PORT = process.env.PORT || 3000;
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App now listening on port:", PORT);
+  });
+});
