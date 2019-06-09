92d96efa91495b0230f2aae247ac03e61709de7f
Heather Peri <heatherhperi@gmail.com>
2019-06-09 11:32:23 -0400

    added js files

---
+++ burgercontroller.js
@@ -0,0 +1,47 @@
+var express = require("express");
+
+var router = express.Router();
+// edit burger model to match sequelize
+var db = require("../models/");
+
+// get route, edited to match sequelize
+router.get("/api/burgers", function(req, res) {
+  // replace old function with sequelize function
+  db.Burger.findAll()
+    // use promise method to pass the burgers...
+    .then(function(burgerData) {
+      res.json(burgerData);
+    });
+});
+
+// post route to create burgers
+router.post("/api/burgers", function(req, res) {
+  // edited burger create to add in a burger_name
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  })
+    // pass the result of our call
+    .then(function(result) {
+      // log the result to our terminal/bash window
+      res.json({ id: result.insertId });
+    });
+});
+
+// put route to devour a burger
+router.put("/api/burgers/:id", function(req, res) {
+  // update one of the burgers
+  db.Burger.update({
+    devoured: true
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }
+  ).then(function(result) {
+    // Send back response and let page reload from .then in Ajax
+    res.sendStatus(200);
+  });
+});
+
+module.exports = router;
